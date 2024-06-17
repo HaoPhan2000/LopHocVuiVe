8cab61c3a30d7f63577183c050d76a1ca81e0391
HaoPhan2000 <trochoivuongquyen30072000@gmail.com>
2024-06-17 23:22:11 +0700

    update config db

--- source/config/db/index.js
+++ source/config/db/index.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/QuanLyLopDayThem");
+    await mongoose.connect((process.env.Mongodb_url),{});
     console.log("connect successfully");
   } catch (error) {
     console.log("connect failure");
   }
 }
 module.exports = { connect };
+//mongodb://localhost:27017/QuanLyLopDayThem
