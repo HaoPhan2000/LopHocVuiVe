80dd28ccd819763a0eb83822fb07e6d026708b20
HaoPhan2000 <trochoivuongquyen30072000@gmail.com>
2024-06-19 00:29:11 +0700

    test code

--- source/middleWares/uploadPhotoMiddleWare.js
+++ source/middleWares/uploadPhotoMiddleWare.js
@@ -3,10 +3,13 @@ const multer = require('multer');
 let appRoot=require('app-root-path');
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
+        console.log(appRoot + '/source/material/assets/img');
         cb(null,appRoot +'/source/material/assets/img');
+        console.log(`Destination Path: ${destPath}`);
     },
     // By default, multer removes file extensions so let's add them back
     filename: function(req, file, cb) {
+        console.log(`Uploading file: ${file.originalname}`);
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
