54996efc987cc02ac3ec818f03694cb784b1c31e
HaoPhan2000 <trochoivuongquyen30072000@gmail.com>
2024-06-19 10:35:08 +0700

    update

--- source/middleWares/uploadPhotoMiddleWare.js
+++ source/middleWares/uploadPhotoMiddleWare.js
@@ -3,13 +3,11 @@ const multer = require('multer');
 let appRoot=require('app-root-path');
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        console.log("Destination Path:"+appRoot + '/source/material/assets/img');
         cb(null,appRoot +'/source/material/assets/img');
        
     },
     // By default, multer removes file extensions so let's add them back
     filename: function(req, file, cb) {
-        console.log(`Uploading file: ${file.originalname}`);
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
